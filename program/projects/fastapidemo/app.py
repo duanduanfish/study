62dca51e1059252195bb9e86f616676bace737da
duansheng <15271853664@163.com>
2024-02-29 18:34:50 +0800

    [fastapidemo] add put method

--- program/projects/fastapidemo/app.py
+++ program/projects/fastapidemo/app.py
@@ -1,6 +1,12 @@
 ﻿from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Item(BaseModel):
+	name: str
+	description: str
+	price: float
 
 @app.get("/")
 def read_root():
@@ -9,3 +15,7 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
 	return {"item_id": item_id, "q": q}
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+	return {"item_name": item.name, "item_id": item_id}
